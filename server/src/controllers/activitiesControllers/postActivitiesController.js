36d2adb76626f1ca1ec6b0b6f87ff921d4ae1d87
FedericoJSosa <federico13javiersosa@gmail.com>
2023-12-12 15:38:50 -0300

    Fin del Back

--- server/src/controllers/activitiesControllers/postActivitiesController.js
+++ server/src/controllers/activitiesControllers/postActivitiesController.js
@@ -1,12 +1,19 @@
-const {Activity}= require("../../db")
+const {Activity, Country}= require("../../db");
+const {Op}= require("sequelize");
+
 
 const postActivitiesController=async({name, difficulty, duration, season, countries})=>{
     const newActivity= await Activity.create({name, difficulty, duration, season})
-    
-    //FUNCIONA PERO TENGO QUE TENER EN CUENTA DE QUE ME VAN A PASAR EL NOMBRE DEL PAIS, NO SU ID
-    //TENDRIA QUE HACER QUE SEGUN EL NOMBRE ME IDENTIFIQUE EL ID Y QUE SEA ESO LO QUE TERMINE PASANDO EN ADD
-    
-    newActivity.addCountries(countries);
+
+    const countriesId= await Country.findAll({
+        where:{
+            name: {[Op.iLike]: countries}
+        },
+        attributes:["id"]
+    })
+
+
+    newActivity.addCountries(countriesId);
     
     
     return newActivity
