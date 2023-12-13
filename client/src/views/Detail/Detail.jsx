5627fc6828442884915140efb82bce41528b2265
FedericoJSosa <federico13javiersosa@gmail.com>
2023-12-13 18:20:13 -0300

    Front

---
+++ client/src/views/Detail/Detail.jsx
@@ -0,0 +1,10 @@
+const Detail=()=>{
+    return (
+        <div>
+            <h1>Soy Detail</h1>
+        </div>
+    )
+};
+
+
+export default Detail;
---
+++ client/src/views/Form/Form.jsx
@@ -0,0 +1,10 @@
+const Form=()=>{
+    return (
+        <div>
+            <h1>Soy Form</h1>
+        </div>
+    )
+};
+
+
+export default Form;
