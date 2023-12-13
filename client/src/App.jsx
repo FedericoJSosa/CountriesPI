666ba3742d342726e00f04bbe8b7fae7095d5bca
FedericoJSosa <federico13javiersosa@gmail.com>
2023-12-13 19:29:09 -0300

    FrontDia1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./views/Home/Home";
 import Landing from "./views/Landing/Landing";
 import Detail from "./views/Detail/Detail";
@@ -8,10 +9,12 @@ import './App.css'
 function App() {
   return (
     <div>
-      <Home></Home>
-      <Detail></Detail>
-      <Form></Form>
-      <Landing></Landing>
+      <Routes>
+        <Route path="/" Component={Landing} />
+        <Route path="/home" Component={Home} />
+        <Route path="/detail" Component={Detail} />
+        <Route path="/form" Component={Form} />
+      </Routes>
     </div>
   )
 }
