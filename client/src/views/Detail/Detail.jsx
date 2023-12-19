a7bc7dcc433529afd6ad444cf72a1c47e5853195
FedericoJSosa <federico13javiersosa@gmail.com>
2023-12-19 16:31:53 -0300

    fr

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -3,30 +3,40 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCountryId, clean } from "../../redux/actions";
 
-const Detail=()=>{
+const Detail = () => {
     const { id } = useParams();
-    const countryId=useSelector(state=>state.countryId);
+    const countryId = useSelector(state => state.countryId);
     const dispatch = useDispatch();
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getCountryId(id));
-        return ()=>{
+        return () => {
             dispatch(clean())
         }
-    },[id])
+    }, [id])
 
-    return (
-        <div>
-            <h1>Soy Detail</h1>
-            <p>ID: {countryId.id}</p>
-            <p>Name: {countryId.name}</p>
-            <img src={countryId.img} alt="Not found" />
-            <p>Continent: {countryId.continent}</p>
-            <p>Capital: {countryId.capital}</p>
-            <p>SubRegion: {countryId.subregion}</p>
-            <p>Area: {countryId.area}</p>
-            <p>Population: {countryId.population}</p>
-        </div>
-    )
+
+    if (countryId.Activities) {
+
+        return (
+            <div>
+                <h1>Soy Detail</h1>
+                <p>ID: {countryId.id}</p>
+                <p>Name: {countryId.name}</p>
+                <img src={countryId.img} alt="Not found" />
+                <p>Continent: {countryId.continent}</p>
+                <p>Capital: {countryId.capital}</p>
+                <p>SubRegion: {countryId.subregion}</p>
+                <p>Area: {countryId.area}</p>
+                <p>Population: {countryId.population}</p>
+                <p>Activities: {countryId.Activities.length !=0 ? 
+                countryId.Activities.map(activity => activity.name).join(", "):
+                "No activities have been assigned yet"}</p>
+            
+            </div>
+        )
+    } else {
+        return <p>Loading...</p>
+    }
 };
 
 
